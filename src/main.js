38031778f793bdbcac80ce1abfec58c5c2d8954f
Lee Young Seo <lupin204@gmail.com>
2018-04-10 18:05:02 +0900

    axios module

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
 
 new Vue({
   el: '#app',
-  render: h => h(App),
+  render: h => h(App)
 
-  // 라우터 객체를 넘겨준다
-  router
 })
